cbda3617bdf82d3f5e3ca2ae36d6073f11547f23
WellBloud <holop@sovanet.cz>
2017-10-23 09:09:23 +0200

    updated with PHP 7.1

--- php5to7.sh
+++ php5to7.sh
@@ -1,5 +1,6 @@
 sudo a2dismod php5.6
-sudo a2enmod php7.0
+sudo a2dismod php7.0
+sudo a2enmod php7.1
 sudo service apache2 restart
-sudo ln -sfn /usr/bin/php7.0 /etc/alternatives/php
-echo "PHP nastaveno na verzi 7.0"
+sudo ln -sfn /usr/bin/php7.1 /etc/alternatives/php
+echo "PHP nastaveno na verzi 7.1 "
